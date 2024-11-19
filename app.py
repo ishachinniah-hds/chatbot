d0024191eecc6980c066508f3beb454f7888e7e5
ishachinniah-hds <isha.chinniah@hitachids.com>
2024-11-19 09:49:14 -0600

    modularized app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from src.document_processor import process_document
 from src.rag_chain import create_rag_chain
 import openlit
-# from transformers import AutoTokenizer
 
 openlit.init(
     otlp_endpoint="http://127.0.0.1:4318"
@@ -14,15 +13,6 @@ load_dotenv()
 def main():
     source = input("Enter the path to the document (PDF, csv, or image): ")
     documents = process_document(source)
-
-    # # Set up Huggingface language model
-    # llm_model = "meta-llama/Llama-3.2-3B-Instruct"
-    # tokenizer = AutoTokenizer.from_pretrained(llm_model)
-
-    # # Number of prompt tokens
-    # encoded_text = tokenizer(result, return_tensors="pt")
-    # token_count = len(encoded_text["input_ids"][0]) 
-    # print(f"Result token count: {token_count}") 
 
     while True:
         question = input("Ask a question about the document (or type 'exit' to quit): ")
