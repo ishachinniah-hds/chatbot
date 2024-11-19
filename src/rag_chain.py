b571a6f124463c544b2a181de95cf49011718f28
ishachinniah-hds <isha.chinniah@hitachids.com>
2024-11-19 09:03:17 -0600

    vector_store modularized

--- src/rag_chain.py
+++ src/rag_chain.py
@@ -2,7 +2,6 @@
 
 from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
-# from langchain_community.vectorstores import FAISS
 from langchain_community.vectorstores import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
@@ -10,12 +9,13 @@ from langchain.embeddings import HuggingFaceBgeEmbeddings
 from langchain_huggingface import HuggingFacePipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 import torch
+from src.vector_store import vector_store
 
 # Load the API key from env variables
 load_dotenv()
 
 RAG_PROMPT_TEMPLATE = """
-You are a helpful coding assistant that can answer questions about the provided context. The context is usually a PDF document or an image (screenshot) of a code file. Augment your answers with code snippets from the context if necessary.
+You are a helpful assistant that can answer questions about the provided context.
 
 If you don't know the answer, say you don't know.
 
@@ -24,29 +24,32 @@ Question: {question}
 """
 PROMPT = PromptTemplate.from_template(RAG_PROMPT_TEMPLATE)
 
-
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 def create_rag_chain(chunks):
-    # Initialize Huggingface embeddings
-    embeddings_model = "sentence-transformers/all-mpnet-base-v2"
-    embeddings = HuggingFaceBgeEmbeddings(model_name=embeddings_model)
-
-    # Set up Chroma vector store
-    doc_search = Chroma.from_documents(chunks, embeddings)
-    retriever = doc_search.as_retriever(
-        search_type="mmr",
-        search_kwargs={'k': 6, 'lambda_mult': 0.25}
-    )
-    # doc_search = FAISS.from_documents(chunks, embeddings)
-    # retriever = doc_search.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+    # Set up the embeddings vector store
+    retriever = vector_store(chunks)
 
     # Set up Huggingface language model
     llm_model = "meta-llama/Llama-3.2-3B-Instruct"
     tokenizer = AutoTokenizer.from_pretrained(llm_model)
     model = AutoModelForCausalLM.from_pretrained(llm_model)
-    hf_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, model_kwargs={"torch_dtype": torch.bfloat16}, device="cpu", max_new_tokens=256,)
+
+    # # Number of prompt tokens
+    # encoded_text = tokenizer(RAG_PROMPT_TEMPLATE, return_tensors="pt")
+    # token_count = len(encoded_text["input_ids"][0]) 
+    # print(f"Prompt token count: {token_count}") 
+
+    hf_pipeline = pipeline(
+        "text-generation", 
+        model=model, 
+        tokenizer=tokenizer, 
+        model_kwargs={"torch_dtype": torch.bfloat16}, 
+        device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+        pad_token_id=tokenizer.eos_token_id,
+        max_new_tokens=256, 
+        )
     llm = HuggingFacePipeline(pipeline=hf_pipeline)
 
     # RAG chain
---
+++ src/vector_store.py
@@ -0,0 +1,23 @@
+# src/vector_store.py
+
+from langchain_community.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+
+def vector_store(chunks, model_name="sentence-transformers/all-mpnet-base-v2"):
+    """
+    Set up ChromaDB vector store given the document chunks. 
+
+    Args:
+        chunks (list): List of document chunks to index
+        model_name (str): The name of the Hugging Face embeddign model to use. 
+
+    Returns:
+        retriever object to retrieve similar documents based on the query. 
+    """
+    embeddings = HuggingFaceBgeEmbeddings(model_name=model_name)
+    doc_search = Chroma.from_documents(chunks, embeddings)
+    retriever = doc_search.as_retriever(
+        search_type="mmr",
+        search_kwargs={'k': 5, 'lambda_mult': 0.25}
+    )
+    return retriever
