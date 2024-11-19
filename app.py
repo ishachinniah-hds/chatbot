25a7b75ecbade75c2915bccce4a7b39feea55987
ishachinniah-hds <isha.chinniah@hitachids.com>
2024-11-19 08:58:01 -0600

    app.py loops to get user inputs

--- app.py
+++ app.py
@@ -2,38 +2,38 @@
 
 from dotenv import load_dotenv
 from src.document_processor import process_document
-from src.rag_chain import create_rag_chain, format_docs
+from src.rag_chain import create_rag_chain
 import openlit
+
+from transformers import AutoTokenizer
 
 openlit.init(
     otlp_endpoint="http://127.0.0.1:4318"
 )
 load_dotenv()
 
-def main():
-    # Get user input
-    source = input("Enter the path to the document (PDF or image): ")
-    question = input("Ask a question about the document (or type 'exit' to quit): ")
-    
-    # Process the document
-    try:
-        chunks = process_document(source)
-        # with open("output.txt", "w") as file:
-        #     print(chunks, file=file)
-    except ValueError as e:
-        print(f"Error processing document: {e}")
-        return
-    
-    # Create RAG chain
-    rag_chain = create_rag_chain(chunks)
+    # # Set up Huggingface language model
+    # llm_model = "meta-llama/Llama-3.2-3B-Instruct"
+    # tokenizer = AutoTokenizer.from_pretrained(llm_model)
 
-    # Run the chain with the question 
-    result = rag_chain.invoke(question)
-    
-    # Output the result
-    print("RAG output: ")
-    print(result)
-    
+    # # Number of prompt tokens
+    # encoded_text = tokenizer(result, return_tensors="pt")
+    # token_count = len(encoded_text["input_ids"][0]) 
+    # print(f"Result token count: {token_count}") 
+
+def main():
+    source = input("Enter the path to the document (PDF, csv, or image): ")
+    documents = process_document(source)
+
+    while True:
+        question = input("Ask a question about the document (or type 'exit' to quit): ")
+        if question.lower() == "exit":
+            break
+
+        # Create rag chain
+        rag_chain = create_rag_chain(documents)
+        answer = rag_chain.invoke(question)
+        print("Answer:", answer)
+
 if __name__ == "__main__":
-    print("starting app.py main()")
     main()
